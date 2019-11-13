29e2773abea3ea213084043f6f2935620ce367d0
nicbritos <nicbritos@hotmail.com>
2019-11-12 23:55:52 -0300

    Added decoding tools (with python)

---
+++ Tools/decodeStrings.py
@@ -0,0 +1,68 @@
+import os
+
+# archivo a cargar tiene que contener pares de numeros en hexa (byte) y deben ser multiplo de 4 (leemos int)
+
+DIVISOR = 0x56B
+
+
+def read_input():
+    try:
+        return input('Write filepath (can be relative) to decode or nothing to exit\nFilepath: ')
+    except SyntaxError:
+        return ''
+
+
+# Doesn't save NULL
+def parse_file(infile, outfile):
+    hex_iterator = (int(c, 16) for c in infile.read())
+
+    try:
+        while True:
+            i = m = n = 0
+            while i < 4:
+                j = b = 0
+                while j < 2:
+                    b |= next(hex_iterator)
+                    if j == 0:
+                        b <<= 4  # Nibble
+                    j += 1
+                m |= b
+                if i < 3:
+                    m <<= 8  # Byte
+                i += 1
+
+            # Invertimos el numero (maldito endianness)
+            i = 0
+            while i < 4:
+                n |= m & 0xff
+                if i < 3:
+                    n <<= 8
+                    m >>= 8
+                i += 1
+
+            if n > 0:
+                n = int(n / DIVISOR)
+                outfile.write(chr(n))
+    except StopIteration:
+        pass
+
+
+def main():
+    input_str = read_input()
+    while input_str is not '':
+        try:
+            with open(input_str, 'r') as infile:
+                output_str = os.path.basename(input_str) + '.dec.txt'
+                with open(output_str, 'w') as outfile:
+                    parse_file(infile, outfile)
+            print('Done "' + input_str + '"!')
+        except Exception as e:
+            print(e)
+
+        input_str = read_input()
+
+    print('Goodbye')
+
+
+if __name__ == '__main__':
+    main()
