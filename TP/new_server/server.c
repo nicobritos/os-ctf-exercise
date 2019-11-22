d4595f7d6b0aeb636ac5a0fe7fb44cf8478a3f4e
nicbritos <nicbritos@hotmail.com>
2019-11-22 15:40:32 -0300

    Barebone server

--- TP/new_server/server.c
+++ TP/new_server/server.c
@@ -1,5 +1,8 @@
 #include "challengesHandler.h"
+#include "clientConnection.h"
+#include <stdint.h>
 #include <string.h>
+#include <stdio.h>
 
 #define PORT 23423
 #define HASH_BUFFER 40
@@ -12,28 +15,28 @@ void main() {
   uint8_t challenge = 0;
   int clientFD;
 
-  clientFD = get_client_connection();
-  while (true) {
+  clientFD = serve(PORT);
+  while (1) {
     system("clear");
-    // exitInMenu11(challenge, stringBuffer);
-    printf("------------- DESAFIO -------------");
-    printf("%s", getChallenge(challenge));
-    if (funciones[(long)challenge] != (void *)0x0) {
-      (*(code *)funciones[(long)challenge])();
-    }
-    puts("\n----- PREGUNTA PARA INVESTIGAR -----");
-    decodeSavedString(preguntasInvestigar + (long)challenge * 0x2c6,stringBuffer);
-    printf("%s",stringBuffer);
-    receive((int)_clientFD,stringBuffer,0x400);
-    hash(stringBuffer,hashBuffer);
-    hashCompareResult = strcmp(hashBuffer,hashes[(long)challenge]);
-    if (hashCompareResult == 0) {
-      puts("Respuesta correcta");
-      challenge += 1;
-    }
-    else {
-      printf("Respuesta incorrecta: %s",stringBuffer);
-    }
+    // // exitInMenu11(challenge, stringBuffer);
+    // printf("------------- DESAFIO -------------");
+    // printf("%s", getChallenge(challenge));
+    // if (funciones[(long)challenge] != (void *)0x0) {
+    //   (*(code *)funciones[(long)challenge])();
+    // }
+    // puts("\n----- PREGUNTA PARA INVESTIGAR -----");
+    // decodeSavedString(preguntasInvestigar + (long)challenge * 0x2c6,stringBuffer);
+    // printf("%s",stringBuffer);
+    receive(clientFD, stringBuffer, STRING_BUFFER);
+    // hash(stringBuffer,hashBuffer);
+    // hashCompareResult = strcmp(hashBuffer,hashes[(long)challenge]);
+    // if (hashCompareResult == 0) {
+    //   puts("Respuesta correcta");
+    //   challenge += 1;
+    // }
+    // else {
+    //   printf("Respuesta incorrecta: %s",stringBuffer);
+    // }
     sleep(1);
   }
 }
